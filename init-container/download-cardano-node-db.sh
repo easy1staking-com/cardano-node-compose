457bd47366b19ce7ce74f148049760c81ff1396b
Giovanni Gargiulo <gargiulo.gianni@gmail.com>
2024-02-12 15:29:30 +0000

    chore: env files

--- init-container/download-cardano-node-db.sh
+++ init-container/download-cardano-node-db.sh
@@ -10,6 +10,8 @@ fi
 if [ ! -d "${CARDANO_NODE_DATA_FOLDER}/db" ]; then
   echo "db missing, downloading"
   rm -fr "${CARDANO_NODE_DATA_FOLDER}/*"
+  echo "AGGREGATOR_ENDPOINT: ${AGGREGATOR_ENDPOINT}"
+  echo "GENESIS_VERIFICATION_KEY: ${GENESIS_VERIFICATION_KEY}"
   mithril-client snapshot download latest --download-dir "${CARDANO_NODE_DATA_FOLDER}"
 else
   echo "data folder already exists. Skipping..."
